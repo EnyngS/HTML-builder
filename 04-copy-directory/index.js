b5133013b247846cb752f40ee1f66cab709a5568
Enyng <susikovvlad@gmail.com>
2022-05-24 12:43:21 +0400

    feat: 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const fs = require("fs");
+const fsPromises = fs.promises;
+function copy(f) {
+  fs.copyFile(
+    `04-copy-directory/files/${f}`,
+    `04-copy-directory/files-copy/${f}`,
+    (err) => {
+      if (err) throw err;
+      console.log(`${f} Успешно скопированн`);
+    }
+  );
+}
 
+fsPromises
+  .mkdir("04-copy-directory/files-copy", { recursive: true })
+  .then(function () {
+    fs.readdir(
+      "04-copy-directory/files",
+      { withFileTypes: true },
+
+      (err, files) => {
+        for (let i = 0; i < files.length; i++) {
+          copy(files[i].name);
+        }
+      }
+    );
+  })
+  .catch(function () {
+    console.log("Ошибка копирования");
+  });
